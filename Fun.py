3ed285b798427fbc127e0d8e2faecca5221350d9
ganglesmunne <air.angles@gmail.com>
2023-02-21 10:10:23 +0100

    compressionloops+changecomb+K=1_condition

--- Fun.py
+++ Fun.py
@@ -1,25 +1,17 @@
 import numpy as np
-import math
 import itertools
 import picos as pic
 import cvxopt as cvx
+from scipy.special import comb
 
 def K_fun(m,k,n):
 
 	Fun=0
 
 	for q in range(0,m+1):
-		Fun+= math.comb(n-k,m-q)*math.comb(k,q)*(-1)**q
+		Fun+= comb(n-k,m-q)*comb(k,q)*(-1)**q
 	return Fun 
 
-def comb_g(n,k):
-
-	if n>=0 and k>=0:
-		value=math.comb(n,k)
-	else:
-		value=0
-
-	return value
 
 def Beta(i,j,k,t,n):
 
@@ -27,7 +19,7 @@ def Beta(i,j,k,t,n):
 
 	for u in range(n+1):
 
-		beta+= comb_g(u,t)*comb_g(n-2*k,u-k)*comb_g(n-k-u,i-u)*comb_g(n-k-u,j-u)*(-1)**(u-t)
+		beta+= comb(u,t)*comb(n-2*k,u-k)*comb(n-k-u,i-u)*comb(n-k-u,j-u)*(-1)**(u-t)
 
 	return beta
 
@@ -38,20 +30,18 @@ def BetaMatrices(Ap,n):
 	for k in range(0,int(n/2)+1):
 
 		Matrix=[[0]*(n-2*k+1) for _ in range(n-2*k+1)]
+		
+		for i, j, t in itertools.product(range(k,n-k+1), range(k,n-k+1), range(n+1)):
 
-		for i in range(k,n-k+1):
-			for j in range(k,n-k+1):
-				for t in range(n+1):
+			l=i+j-2*t
 
-					l=i+j-2*t
+			if l>n or t>i or t>j:
 
-					if l>n or t>i or t>j:
+				Matrix[i-k][j-k]+=0
 
-						Matrix[i-k][j-k]+=0
+			else:
 
-					else:
-
-						Matrix[i-k][j-k]+=(1/math.comb(n,l))*Ap[l]*Beta(i,j,k,t,n)*(math.comb(n-2*k,i-k)*math.comb(n-2*k,j-k))**(-0.5)
+				Matrix[i-k][j-k]+=(1/comb(n,l))*Ap[l]*Beta(i,j,k,t,n)*(comb(n-2*k,i-k)*comb(n-2*k,j-k))**(-0.5)
 
 		BMatrix.append(Matrix)
 
@@ -71,16 +61,15 @@ def GammaTilde(Ap,n):
 
 	GTilde=[[0]*(2**n) for _ in range(2**n)]
 	
-	for S in Elements:
-		for T in Elements:
-
-			i=len(S)
-			j=len(T)
-			t=len(tuple(set(S) & set(T)))
-			
-			l=i+j-2*t
-			
-			GTilde[Elements_R[S]][Elements_R[T]]=(1/math.comb(n,l))*Ap[l]
+	for S,T in itertools.product(Elements,Elements):
+		
+		i=len(S)
+		j=len(T)
+		t=len(tuple(set(S) & set(T)))
+		
+		l=i+j-2*t
+		
+		GTilde[Elements_R[S]][Elements_R[T]]=(1/comb(n,l))*Ap[l]
 
 	return GTilde
 
